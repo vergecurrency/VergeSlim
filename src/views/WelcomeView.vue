09a5380649e7b44e421a1e00a4545219b59bc3eb
justinvforvendetta <justinvforvendetta@gmail.com>
2026-03-12 20:00:29 -0400

    Refine welcome screen header layout

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -2,12 +2,11 @@
   <div>
     <div class="columns">
       <div class="column welcome-header">
+        <div class="welcome-copy has-text-right">
+          <h1 class="welcome-title" v-html="$i18n.t('welcome.welcomeToMyVergies')"/>
+        </div>
         <div class="header-logo">
           <img src="@/assets/headers/logo@2x.png"/>
-        </div>
-        <div class="has-text-right">
-          <h1 class="is-size-1 is-family-handwritten" v-html="$i18n.t('welcome.welcomeToMyVergies')"/>
-          <h4 class="is-size-6 has-text-grey" v-html="$i18n.t('welcome.createdBy')"/>
         </div>
       </div>
     </div>
@@ -58,9 +57,46 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+    gap: 2rem;
   }
 
   .header-logo > img {
     width: 200px;
+  }
+
+  .welcome-copy {
+    min-width: 340px;
+  }
+
+  .welcome-title {
+    font-family: "Avenir Next", Futura, "Trebuchet MS", sans-serif;
+    font-size: 3.5rem;
+    font-weight: 800;
+    letter-spacing: -0.04em;
+    line-height: 0.95;
+    color: #2f3a44;
+    text-wrap: balance;
+  }
+
+  @media (max-width: 768px) {
+    .welcome-header {
+      flex-direction: column-reverse;
+      gap: 1rem;
+    }
+
+    .welcome-title {
+      font-size: 2.75rem;
+      text-align: center;
+    }
+
+    .welcome-copy {
+      min-width: 0;
+    }
+  }
+
+  @media (prefers-color-scheme: dark) {
+    .welcome-title {
+      color: #f4f6f8;
+    }
   }
 </style>
