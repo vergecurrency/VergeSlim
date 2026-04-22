74dbc62b646c879c6483f9e197d83445b2b98b25
sunerok <justinvforvendetta@gmail.com>
2026-04-22 17:07:16 -0400

    Remove unused files

--- src/toolbar/menu.ts
+++ src/toolbar/menu.ts
@@ -1,23 +1,4 @@
-import { MenuItem } from 'electron'
-import HelpMenu from './help'
-import WindowMenu from './window'
-import ViewMenu from './view'
-import ServicesMenu from './services'
-import AppMenu from './app'
-import { isMacOSEnvironment } from '@/utils'
-
 /**
  * macOS Dock menu items
  */
 export const dockTemplate = []
-
-/**
- * Application menu.
- */
-export const generateMenuTemplate: () => MenuItem[] = () => [
-  ...(isMacOSEnvironment() ? [AppMenu] : []),
-  ServicesMenu,
-  ViewMenu,
-  WindowMenu,
-  HelpMenu
-]
